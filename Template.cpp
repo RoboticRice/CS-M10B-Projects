6547cbbae735512c3cbe8c78f7f2d13611ed2198
Sam Rice <sammuelrice@gmail.com>
2016-01-21 06:18:19 +0000

    Didn't add template, trying again

---
+++ Template.cpp
@@ -0,0 +1,12 @@
+using namespace std;
+/*
+// Project: Template
+// Class:   CS M10B
+// Student: Rice, Sammuel D.
+// Date:    20160120
+// gitHub:  https://github.com/RoboticRice/CS-M10B-Projects.git
+*/
+int main() {
+    //Main class
+    return 0;
+}
