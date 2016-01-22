5697fc2fdd6ebe02efa436a63b377fe42457c983
Sam Rice <sammuelrice@gmail.com>
2016-01-22 03:13:59 +0000

    Running out of battery, want to push this 1st

--- HW2.cpp
+++ HW2.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 using namespace std;
 /*
 * Project: Homework #2
@@ -15,7 +16,38 @@ using namespace std;
 * 
 */
 
+class Animal
+{
+    private:
+    int Age;
+    
+    public:
+    int Length;
+    
+    void setAge(int years) //Mutators
+    {
+        Age = years;
+    }
+    
+    int getAge() const; //Accessors
+    
+    Length = 4;
+};
+
+int Animal::getAge() const
+{
+    return Age;//*inDays;
+}
+
 int main() {
     //Main class
+    Animal Tiger;
+    // cout << "Tiger Length: ";
+    // cin >> Tiger.Length;
+    int temp = 0;
+    cout << "Tiger Age: ";
+    cin >> temp;
+    Tiger.setAge(temp);
+    cout << "Length = " << Tiger.Length << " :: Age = " << Tiger.getAge() << endl;
     return 0;
 }
