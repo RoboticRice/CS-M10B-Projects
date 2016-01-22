927c943db50b7a5695737ec21424fe49098402c5
Sam Rice <sammuelrice@gmail.com>
2016-01-22 01:49:05 +0000

    Corrected template, imported template to HW2

--- HW2.cpp
+++ HW2.cpp
@@ -1,11 +1,20 @@
 using namespace std;
 /*
-// Project: Homework #2
-// Class:   CS M10B
-// Student: Rice, Sammuel D.
-// Date:    201601##
-// gitHub:  https://github.com/RoboticRice/CS-M10B-Projects.git
+* Project: Homework #2
+* Class:   CS M10B
+* Student: Rice, Sammuel D.
+* Date:    20160121
+* gitHub:  https://github.com/RoboticRice/CS-M10B-Projects.git
 */
+
+/* 
+* Instructions:
+*
+*//*
+* Notes:
+* 
+*/
+
 int main() {
     //Main class
     return 0;
--- Template.cpp
+++ Template.cpp
@@ -1,11 +1,20 @@
 using namespace std;
 /*
-// Project: Template
-// Class:   CS M10B
-// Student: Rice, Sammuel D.
-// Date:    20160120
-// gitHub:  https://github.com/RoboticRice/CS-M10B-Projects.git
+* Project: Template
+* Class:   CS M10B
+* Student: Rice, Sammuel D.
+* Date:    20160120
+* gitHub:  https://github.com/RoboticRice/CS-M10B-Projects.git
 */
+
+/* 
+* Instructions:
+*
+*//*
+* Notes:
+* 
+*/
+
 int main() {
     //Main class
     return 0;
