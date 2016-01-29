c6c0504c59d53cc95c5af8c617c0ad14eff68879
Sam Rice <sammuelrice@gmail.com>
2016-01-29 02:14:16 +0000

    Added HW#3

---
+++ HW3.cpp
@@ -0,0 +1,23 @@
+using namespace std;
+//#include <iostream>
+//#include <string>
+/*
+* Project: Homework #3
+* Class:   CS M10B
+* Student: Rice, Sammuel D.
+* Date:    20160128
+* gitHub:  https://github.com/RoboticRice/CS-M10B-Projects.git
+*/
+
+/* 
+* Instructions:
+*
+*//*
+* Notes:
+* 
+*/
+
+int main() {
+    //Main class
+    return 0;
+}
