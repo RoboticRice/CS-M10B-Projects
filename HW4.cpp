a46cd658ae32e7185c8d0c81a373343d3139cc23
Sam Rice <sammuelrice@gmail.com>
2016-02-13 21:39:46 +0000

    Finished HW4 w/ recursive functions. Fun

--- HW4.cpp
+++ HW4.cpp
@@ -1,6 +1,7 @@
 using namespace std;
 #include <iostream>
 #include <string>
+#include <algorithm> //This is for tolower()
 /*
 * Project: Homework #4
 * Version: 0.1 (History: 0.1)
@@ -18,28 +19,60 @@ using namespace std;
 * This program has built in mechanics to remove capitalization and gramar. 
 */
 
-bool Palindrome(string phrase);
-bool Palindrome(string phrase, int location);
+string RemoveChar(string phrase, int location);
+bool   Palindrome(string phrase, int location);
 
 int main() {
     //Main class
+    
     string input;
     cout << "This program checks for Palindromes." << endl;
     cout << "Please enter what you would like checked: ";
-    cin >> input;
-    if (Palindrome(input)) cout << "That is a palindrome." << endl;
+    getline(cin, input); //This is used instead of a normal cin so spaces can be input
+    
+    //First, get rid of any capitals
+    transform(input.begin(), input.end(), input.begin(), ::tolower);
+    
+    //Then, remove special characters using
+    //RemoveChar(input, 0);
+    //And check if what that returns is a Palindrome inline:
+    if (Palindrome(RemoveChar(input, 0), 0)) cout << "That is a palindrome." << endl;
     else cout << "That is NOT a palindrome." << endl;
     return 0;
 }
 
-bool Palindrome(string phrase)
+string RemoveChar(string phrase, int location)
 {
-    cout << phrase << endl;
-    return true;
+    if (location < phrase.size())
+    {
+        //If Number or Lower Case Letter:
+        if ((phrase[location] >= 48 && phrase[location] <= 57) || (phrase[location] >= 97 && phrase[location] <= 122))
+        {
+            location++;
+        } else {
+            phrase.erase(location, 1); //This removes the actual locations of the char, so there's no need to local++
+        }
+        //For debuging:
+        //cout << phrase[location] << " " << location << " " << phrase << " " << phrase.size() << endl;
+        return RemoveChar(phrase, location);
+    } else {
+        return phrase;
+    }
 }
 
 bool Palindrome(string phrase, int location)
 {
-    cout << phrase << " " << location << endl;
-    return true;
+    if (phrase[location] == phrase[phrase.size()-location-1])
+    {
+        //if we have not yet reached the halfway point
+        if (phrase.size()/2 > location) 
+        {
+            location++;
+            return Palindrome(phrase, location);
+        } else {
+            return true;
+        }
+    } else {
+        return false;
+    }
 }
